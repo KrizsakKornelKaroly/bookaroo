c0f17ddabd03b1cab27a29acb41dd5311dc86ba2
OhradkaGergo <ohradkagergo@turr.hu>
2025-12-09 09:25:45 +0100

    BK-6 - Registration form implemented

--- Bookaroo_fe/src/app/app.routes.ts
+++ Bookaroo_fe/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'registration',
+        component: RegistrationComponent
+    },
+];
