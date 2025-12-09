6e023703d56294854aea67a8dc05c890fd571144
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2025-12-09 11:35:09 +0100

    BK-7 - Main page update + PrimeNG implementation

--- Bookaroo_fe/src/app/components/system/main/main.component.ts
+++ Bookaroo_fe/src/app/components/system/main/main.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { DatePicker } from 'primeng/datepicker';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule, FloatLabelModule, DatePicker, InputNumberModule, ButtonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
