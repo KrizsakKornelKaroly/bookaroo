f6528cae73a7d7d399fb87264289cad574b068ee
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2025-12-09 10:24:14 +0100

    BK-7 - Main page creation + try 1 to impl. PrimeNG

--- Bookaroo_fe/src/app/app.routes.ts
+++ Bookaroo_fe/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/user/login/login.component';
 import { RegistrationComponent } from './components/user/registration/registration.component';
+import { MainComponent } from './components/system/main/main.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: MainComponent
+    },
     {
         path: 'login',
         component: LoginComponent
