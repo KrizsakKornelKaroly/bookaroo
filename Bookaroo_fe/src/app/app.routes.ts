fd5919577c989c5644dc702e2dc40680a7350ae9
OhradkaGergo <ohradkagergo@turr.hu>
2025-12-09 11:49:29 +0100

    BK-9 - Not found implementation

--- Bookaroo_fe/src/app/app.routes.ts
+++ Bookaroo_fe/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/user/login/login.component';
 import { RegistrationComponent } from './components/user/registration/registration.component';
 import { MainComponent } from './components/system/main/main.component';
+import { NotfoundComponent } from './components/system/notfound/notfound.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,11 @@ export const routes: Routes = [
     {
         path: 'registration',
         component: RegistrationComponent
+    },
+
+
+    {
+        path: '**',
+        component: NotfoundComponent
     },
 ];
