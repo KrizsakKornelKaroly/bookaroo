5d3fcbc447e5507583dd413a2f49d849787fd47d
OhradkaGergo <ohradkagergo@turr.hu>
2025-12-15 10:33:41 +0100

    BK-10 - Accommodation list implementation WIP, general PrimeNG select border color overwrite

--- Bookaroo_fe/src/app/components/user/accommodation-list/accommodation-list.component.ts
+++ Bookaroo_fe/src/app/components/user/accommodation-list/accommodation-list.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'app-accommodation-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, SelectModule],
   templateUrl: './accommodation-list.component.html',
-  styleUrl: './accommodation-list.component.scss'
+  styleUrls: ['./accommodation-list.component.scss']
 })
-export class AccommodationListComponent {
-  
+export class AccommodationListComponent implements OnInit {
+  countries: any[] | undefined;
+  cities: any[] | undefined;
+
+  selectedCountry: string | undefined;
+  selectedCity: string | undefined; // added
+
+  ngOnInit() {
+
+    this.countries = [
+      { name: 'Australia', code: 'AU' },
+      { name: 'Brazil', code: 'BR' },
+      { name: 'China', code: 'CN' },
+      { name: 'Egypt', code: 'EG' },
+      { name: 'France', code: 'FR' },
+      { name: 'Germany', code: 'DE' },
+      { name: 'India', code: 'IN' },
+      { name: 'Japan', code: 'JP' },
+      { name: 'Spain', code: 'ES' },
+      { name: 'United States', code: 'US' }
+    ]
+
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Los Angeles', code: 'LA' },
+      { name: 'Chicago', code: 'CH' },
+      { name: 'Houston', code: 'HO' },
+      { name: 'Phoenix', code: 'PH' }
+    ]
+  }
 }
