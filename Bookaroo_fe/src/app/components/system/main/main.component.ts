8e5bbd92b5405189b45f7b23cb8a68906d4d1c7e
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2025-12-15 10:32:39 +0100

    BK-7 - Main page expansion

--- Bookaroo_fe/src/app/components/system/main/main.component.ts
+++ Bookaroo_fe/src/app/components/system/main/main.component.ts
@@ -10,10 +10,10 @@ import { CarouselModule } from 'primeng/carousel';
   selector: 'app-main',
   standalone: true,
   imports: [
-    InputTextModule, 
-    FloatLabelModule, 
-    DatePicker, 
-    InputNumberModule, 
+    InputTextModule,
+    FloatLabelModule,
+    DatePicker,
+    InputNumberModule,
     ButtonModule,
     CarouselModule
   ],
@@ -21,39 +21,48 @@ import { CarouselModule } from 'primeng/carousel';
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
-  
+
 
   responsiveOptions = [
     {
-        breakpoint: '1400px',
-        numVisible: 2,
-        numScroll: 1,
+      breakpoint: '1400px',
+      numVisible: 2,
+      numScroll: 1,
     },
     {
-        breakpoint: '1199px',
-        numVisible: 3,
-        numScroll: 1,
+      breakpoint: '1199px',
+      numVisible: 3,
+      numScroll: 1,
     },
     {
-        breakpoint: '767px',
-        numVisible: 2,
-        numScroll: 1,
+      breakpoint: '767px',
+      numVisible: 1,
+      numScroll: 5,
     },
     {
-        breakpoint: '575px',
-        numVisible: 1,
-        numScroll: 1,
+      breakpoint: '575px',
+      numVisible: 1,
+      numScroll: 5,
     },
-];
+  ];
 
-products = [
-    {name: 'Szállás 1', image: 'https://placehold.co/300x150'},
-    {name: 'Szállás 2', image: 'https://placehold.co/300x150'},
-    {name: 'Szállás 3', image: 'https://placehold.co/300x150'},
-    {name: 'Szállás 4', image: 'https://placehold.co/300x150'},
-    {name: 'Szállás 5', image: 'https://placehold.co/300x150'},
-    {name: 'Szállás 6', image: 'https://placehold.co/300x150'},
-];
+  cities = [
+    { name: 'Szállás 1', image: 'https://placehold.co/300x150' },
+    { name: 'Szállás 2', image: 'https://placehold.co/300x150' },
+    { name: 'Szállás 3', image: 'https://placehold.co/300x150' },
+    { name: 'Szállás 4', image: 'https://placehold.co/300x150' },
+    { name: 'Szállás 5', image: 'https://placehold.co/300x150' },
+    { name: 'Szállás 6', image: 'https://placehold.co/300x150' },
+  ];
+
+  accomodations = [
+    { name: 'Szállás 1', address: "A", capacity: "2", basePrice: "25000", image: 'https://placehold.co/300x325' },
+    { name: 'Szállás 2', address: "B", capacity: "3", basePrice: "120000", image: 'https://placehold.co/300x325' },
+    { name: 'Szállás 3', address: "C", capacity: "6", basePrice: "12000", image: 'https://placehold.co/300x325' },
+    { name: 'Szállás 4', address: "D", capacity: "4", basePrice: "20000", image: 'https://placehold.co/300x325' },
+    { name: 'Szállás 5', address: "E", capacity: "1", basePrice: "78000", image: 'https://placehold.co/300x325' },
+    { name: 'Szállás 6', address: "F", capacity: "2", basePrice: "6800", image: 'https://placehold.co/300x325' },
+  ];
+
 
 }
-
