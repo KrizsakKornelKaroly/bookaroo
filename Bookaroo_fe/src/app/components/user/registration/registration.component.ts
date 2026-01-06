530bbf3cf8fa7547c6c1e4d1eac35739c211d423
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2026-01-06 09:40:34 +0100

    BK-18, BK-19 - auth update

--- Bookaroo_fe/src/app/components/user/registration/registration.component.ts
+++ Bookaroo_fe/src/app/components/user/registration/registration.component.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ApiService } from '../../../services/api.service';
+import { User } from '../../../interfaces/user';
+import { MessageComponent } from "../../system/message/message.component";
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MessageComponent],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
 
+  constructor(
+    private api: ApiService
+  ) { }
+
+  acceptedTerms : boolean = false;
+  newUser : User = {
+    name: '',
+    email: '',
+    password: '',
+    confirm: '',
+    role: ''
+  };
 
   registration() {
-    
+    if (!this.acceptedTerms){
+
+    }
   }
 }
